8a6b228cc233623623c3e07cc01bdc1a58940f66
Hamilton do Vale Oliveira <hamilton.valnet@gmail.com>
2025-06-05 14:35:12 -0300

    spinner painel

--- src/app/component/painel/painel.component.ts
+++ src/app/component/painel/painel.component.ts
@@ -10,38 +10,35 @@ import { PainelService } from 'src/services/painel.service';
 export class PainelComponent {
 
    Documentacao: DocumentoMSG[] = [];
-
+   carregando = false;
+     
   constructor(private painelService: PainelService) { 
    this. carregaDocumentos();
   }
 
-  calcularPreco(bytes: number): string {
-  const precoPorMB = 25.00
-  const mb = bytes / (1024 * 1024);
-  let preco = mb * precoPorMB;
-  preco = Math.max(preco, 0.01);
-  return preco.toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-    minimumFractionDigits: 2
-  });
-}
+  spinner(valor: boolean) {
+    this.carregando = valor;
+    if (valor) {
+      setTimeout(() => {
+        this.carregando = false;
+      }, 1000);
+    }
+  }
 
-  carregaDocumentos(): void {
+   carregaDocumentos(): void {
       const guidCliente = localStorage.getItem('guidCliente');
       if (!guidCliente) return;
-      //this.spinner(true);
       this.painelService.listarMGS(guidCliente).subscribe({
         next: (dados: DocumentoMSG[]) => {
           this.Documentacao = dados ?? [];
           if (!dados || dados.length === 0) {
             //this.toastr.warning('Atenção: esta fila ainda não possui configuração de documentos.');
           }
-          //this.spinner(false);
+          this.spinner(false);
         },
         error: (erro) => {
           //this.Erro('Erro ao carregar configurações de documento:');
-          //this.spinner(false);
+          this.spinner(false);
         }
       });
     }
