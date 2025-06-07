b78b29d9035279d8ecf57dc8d5ba671069fb1009
Hamilton do Vale Oliveira <hamilton.valnet@gmail.com>
2025-06-07 13:58:09 -0300

    menu autorização

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Observable, Subscribable } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
+  
 export class MenuComponent {
+ logado = false;
+
+ 
+  constructor() {
+    if (localStorage.getItem('token')) {
+      this.logado =true;
+    }
+
+  }
 
 }
