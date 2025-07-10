4b14bfeb38d256513b0efe295f5c3c5e6d10e433
Hamilton do Vale Oliveira <hamilton.valnet@gmail.com>
2025-07-10 14:16:41 -0300

    build e configuração

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,32 +4,23 @@ import { MenuComponent } from './menu/menu.component';
 import { ContratoComponent } from './contrato/contrato.component';
 
 const routes: Routes = [
-{
-    path: '',
-    component: ContratoComponent, // Rota raiz carrega esse componente
-    pathMatch: 'full'
-  },
-  {
-    path: 'contrato',
-    component: ContratoComponent,
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () =>
-      import('./component/login/login.module').then((m) => m.LoginModule),
-  },
-  {
-    path: 'menu',
-    component: MenuComponent,
-  },
+  // Rota raiz carregando componente direto
+  { path: '', component: ContratoComponent, pathMatch: 'full' },
+
+  // Rotas com componente carregado diretamente
+  { path: 'menu', component: MenuComponent },
+
+  // Rotas com lazy loading
+  { path: 'login', loadChildren: () => import('./component/login/login.module').then(m => m.LoginModule) },
   { path: 'cadastro-empresa', loadChildren: () => import('./component/cadastro-empresa/cadastro-empresa.module').then(m => m.CadastroEmpresaModule) },
   { path: 'configuracao-documento', loadChildren: () => import('./component/configuracao-documento/configuracao-documento.module').then(m => m.ConfiguracaoDocumentoModule) },
   { path: 'instrucoes', loadChildren: () => import('./component/instrucoes/instrucoes.module').then(m => m.InstrucoesModule) },
   { path: 'painel', loadChildren: () => import('./component/painel/painel.module').then(m => m.PainelModule) },
-  { path: 'chamado', loadChildren: () => import('./component/chamado/chamado.module').then(m => m.ChamadoModule) }, 
-];
+  { path: 'chamado', loadChildren: () => import('./component/chamado/chamado.module').then(m => m.ChamadoModule) },
 
+  // Rota coringa: redireciona para a raiz
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
